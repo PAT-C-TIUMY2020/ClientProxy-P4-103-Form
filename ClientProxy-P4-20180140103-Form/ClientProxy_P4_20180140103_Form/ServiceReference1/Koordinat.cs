7a36eee8bfbf64c446b167b7af8fdc4def01fa65
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-10-30 19:50:59 +0700

    Add project files.

---
+++ ClientProxy-P4-20180140103-Form/ClientProxy_P4_20180140103_Form/ServiceReference1/Koordinat.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientProxy_P4_20180140103_Form.ServiceReference1
+{
+    internal class Koordinat
+    {
+        public int X { get; internal set; }
+        public int Y { get; internal set; }
+    }
+}
